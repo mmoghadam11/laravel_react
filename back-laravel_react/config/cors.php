8cb9eaa0e401ee0adae94bf196c77a9cd22c69ad
Mahdiyar <mahyar_moghadam11@yahoo.ca>
2022-08-21 10:27:20 -0700

    mongodb schema

--- back-laravel_react/config/cors.php
+++ back-laravel_react/config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
