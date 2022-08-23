038842a164b41fac07a52eca73720280186bc3ab
Mahdiyar <mahyar_moghadam11@yahoo.ca>
2022-08-23 14:50:29 -0700

    initial react base

---
+++ front_laravel_react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>hi</Button>
+    </div>
+  );
+}
+
+export default App;
